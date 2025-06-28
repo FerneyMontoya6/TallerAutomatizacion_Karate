a4620ac44c9fff5cf9d8eb92c785d992ee1b2c75
FerneyMontoya6 <63945368+FerneyMontoya6@users.noreply.github.com>
2025-06-28 09:42:43 -0500

    Update TestRunner.java

--- src/test/java/org/udea/parabank/TestRunner.java
+++ src/test/java/org/udea/parabank/TestRunner.java
@@ -5,7 +5,17 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
 
     @Karate.Test
-    Karate runAll() {
-        return Karate.run().relativeTo(getClass());
+    Karate test01_ContactAppLogin() {
+        return Karate.run("login")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
+
+    @Karate.Test
+    Karate test02_ContactAppCreateContact() {
+        return Karate.run("CreateContact")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }    
+
 }
